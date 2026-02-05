566e4fd373dac91d7b5bb60a14d08f79c51b8667
Marc Jeffson Delos Santos <marc.delossantos@awsys-i.com>
2026-02-05 15:37:31 +0800

    update
    update import for runalltest file

--- runAllTest.js
+++ runAllTest.js
@@ -1,14 +1,19 @@
 const { loginTest } = require('./tests/loginTest');
 const { dataCreationIA } = require('./tests/createNewIA');
+const { updateDataIA } = require('./tests/updateIA');
+const { filterData } = require('./tests/IAFilteringData');
 
 (async () => {
-   // await loginTest();
+    await loginTest();
     console.log('test_001 finished----you may check report logs @excelLog folder-----'); 
 
     await dataCreationIA();
     console.log('test_002 finished----you may check report logs @excelLog folder-----');
 
-    // await updateDataIA();
+    await updateDataIA();
+    console.log('test_003 finished----you may check report logs @excelLog folder-----');
+
+    await filterData();
     console.log('test_003 finished----you may check report logs @excelLog folder-----');
 })();
     
